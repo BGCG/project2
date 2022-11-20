3957740cd0ebf1ef005f6996a51b1fcfa7a30947
BGCG <billie.griffith4@gmail.com>
2022-11-20 11:43:45 +0000

    Addition of answer counting function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
 let buttons = document.getElementsByTagName("button");
 for (let button of buttons) {
     button.addEventListener("click", function() {
-        if (this.getElementsByTagName("button") === "click") {
+        if (document.getElementById("score1") === "click") {
             alert("You clicked the sledgehammer!");
         } else {
             scorecounting();
@@ -19,15 +19,34 @@ function scorecounting() {
 
 console.log("Score counting...");
 
-document.getElementById("score1") = 1;
-document.getElementById("score2") = 2;
-document.getElementById("score3") = 3;
-document.getElementById("score4") = 4;
-document.getElementById("score5") = 5;
+let score1 = document.getElementById("score1");
+let score2 = document.getElementById("score2");
+let score3 = document.getElementById("score3");
+let score4 = document.getElementById("score4");
+let score5 = document.getElementById("score5");
 
 }
+
+let score = 0;
+for (i = 0; score.length; i++)
+
+
+
 
 function finalscoreaddition() {
-
+  let totalScore = (q1.score + q2.score + q3.score + q4.score);
 }
+
+if (SubmitEvent) {    
+    if (totalScore > 6) {
+        alert("You are a bubbly personality")
+    } else if (totalScore > 10) {
+            alert("You are highly ambitious!")
+        }
+        else if (totalscore < 6)
+        alert("You are shy by nature!")
+    } else {
+        alert("You are a complex person with many different conflicting traits and desires!")
+    }
+    
 
