323655997871af77267659db6e0f4102451d4762
BGCG <billie.griffith4@gmail.com>
2022-11-20 10:49:27 +0000

    Addition of functions in js - working progress..

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,30 @@ document.addEventListener("DOMContentLoaded", function() {
 let buttons = document.getElementsByTagName("button");
 for (let button of buttons) {
     button.addEventListener("click", function() {
-        if (this.getAttribute("data-type") === "click") {
+        if (this.getElementsByTagName("button") === "click") {
             alert("You clicked the sledgehammer!");
         } else {
-            alert("You missed");
+            scorecounting();
         }
     });
 }
 });
+
+
+
+function scorecounting() {
+
+console.log("Score counting...");
+
+document.getElementById("score1") = 1;
+document.getElementById("score2") = 2;
+document.getElementById("score3") = 3;
+document.getElementById("score4") = 4;
+document.getElementById("score5") = 5;
+
+}
+
+function finalscoreaddition() {
+
+}
 
