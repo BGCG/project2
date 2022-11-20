aca07e74fc9e42e1b68153b25661f5f972ca252f
BGCG <billie.griffith4@gmail.com>
2022-11-20 08:09:39 +0000

    Addition of fontawesome icons and alert scripts to JS and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 console.log('Hello world');
+
+document.addEventListener("DOMContentLoaded", function() {
+let buttons = document.getElementsByTagName("button");
+for (let button of buttons) {
+    button.addEventListener("click", function() {
+        if (this.getAttribute("data-type") === "click") {
+            alert("You clicked the sledgehammer!");
+        } else {
+            alert("You missed");
+        }
+    });
+}
+});
+
